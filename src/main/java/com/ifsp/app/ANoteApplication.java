c77417df9ed4558ca3a7bb7302ddb3ddf8adbdcd
João Pedro <jpsdb10@hotmail.com>
2025-05-29 08:42:21 -0300

    spring boot

---
+++ src/main/java/com/ifsp/app/ANoteApplication.java
@@ -0,0 +1,13 @@
+package com.ifsp.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ANoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ANoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ifsp/app/ANoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifsp.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ANoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
